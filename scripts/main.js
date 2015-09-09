ba264b356dd58b3c0d81687206fa4a5829cb5ac5
jeffsil <jeff_s_il@yahoo.com>
2015-09-09 16:37:55 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
 	  var myName = prompt('Hi:  Please enter your name.');
 	  localStorage.setItem('name', myName);
-	  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+	  myHeading.innerHTML = 'Mozilla is super cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
